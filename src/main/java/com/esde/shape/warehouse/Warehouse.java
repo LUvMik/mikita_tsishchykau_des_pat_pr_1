c7bf9e0794a8799846e7949c6e5a60a6f9aa3faf
m.tsishchykau <m.tishchykau@besmart.by>
2025-05-19 12:41:53 +0300

    Code review

--- src/main/java/com/esde/shape/warehouse/Warehouse.java
+++ src/main/java/com/esde/shape/warehouse/Warehouse.java
@@ -1,7 +1,6 @@
 package com.esde.shape.warehouse;
 
 import com.esde.shape.entity.Cone;
-import com.esde.shape.warehouse.impl.WarehouseImpl;
 
 public interface Warehouse {
     void update(Cone cone, double volume, double surfaceArea);
